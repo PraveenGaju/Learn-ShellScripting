03fa774023c4ee79c8082d0201189efe15c15fa0
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-03-29 20:59:59 +0530

    updating Learn-ShellScripting directory

--- 03.Variable.sh
+++ 03.Variable.sh
@@ -4,3 +4,17 @@ echo $0 # script name
 echo $* #all arguments
 echo $@ #all arguments
 echo $# #number of arguments
+DATE=2023-02-17
+echo "Hello, Good Morning, Today Date is ${DATE}"
+
+# Command Substitution
+DATE=$(date)
+echo "Hello, Good Morning, Today Date is ${DATE}"
+
+# Arithmetic Substitution
+ADD=$((2+3))
+echo "Addition of 2+3 = ${ADD}"
+
+
+## Accessing Variable from Shell
+echo x = $x
